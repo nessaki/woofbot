258c6380476a1ada64742b3f6a1bb9c248a8ab0d
Latif Khalifa <latifer@streamgrid.net>
2010-04-30 22:49:57 +0000

    First successful xmpp connect
    --HG--
    extra : convert_revision : svn%3A866b3482-8af3-4336-98a2-0cf50ee3e9ce/trunk%4016

--- XmppBot.cs
+++ XmppBot.cs
@@ -32,19 +32,22 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using jabber;
 using jabber.client;
 using jabber.connection;
 
 namespace Jarilo
 {
-    public class XmppBot: IDisposable, IRelay
+    public class XmppBot : IDisposable, IRelay
     {
         Program MainProgram;
         public XmppServerInfo Conf;
         Configuration MainConf;
         public JabberClient Client;
         public StanzaStream Stream;
+        public ConferenceManager ConfManager;
+        public List<Room> Conferences = new List<Room>();
 
         public XmppBot(Program p, XmppServerInfo c, Configuration m)
         {
@@ -56,6 +59,14 @@ namespace Jarilo
 
         public void Dispose()
         {
+            Conferences.Clear();
+
+            if (ConfManager != null)
+            {
+                ConfManager.Dispose();
+                ConfManager = null;
+            }
+
             if (Stream != null)
             {
                 if (Stream.Connected)
@@ -72,6 +83,14 @@ namespace Jarilo
 
         public void Connect()
         {
+            Conferences.Clear();
+
+            if (ConfManager != null)
+            {
+                ConfManager.Dispose();
+                ConfManager = null;
+            }
+
             if (Stream != null)
             {
                 if (Stream.Connected)
@@ -85,17 +104,102 @@ namespace Jarilo
             Client = new JabberClient();
             Client.OnInvalidCertificate += new System.Net.Security.RemoteCertificateValidationCallback(Client_OnInvalidCertificate);
             Client.OnConnect += new jabber.connection.StanzaStreamHandler(Client_OnConnect);
+            Client.OnReadText += new bedrock.TextHandler(Client_OnReadText);
+            Client.OnWriteText += new bedrock.TextHandler(Client_OnWriteText);
+            Client.OnError += new bedrock.ExceptionHandler(Client_OnError);
+            Client.OnStreamError += new jabber.protocol.ProtocolHandler(Client_OnStreamError);
+            Client.OnAuthenticate += new bedrock.ObjectHandler(Client_OnAuthenticate);
+            Client.OnAuthError += new jabber.protocol.ProtocolHandler(Client_OnAuthError);
+            ConfManager = new ConferenceManager();
+            ConfManager.Stream = Client;
+
             Client.User = Conf.User;
             Client.Server = Conf.Domain;
             Client.NetworkHost = Conf.NetworkHost;
             Client.Password = Conf.Password;
+            Client.KeepAlive = 30f;
+            Client.AutoLogin = true;
+            Client.AutoStartTLS = true;
+            Client.AutoReconnect = 15f;
+            Client.AutoPresence = true;
+            Client.AutoIQErrors = true;
             Client.Connect();
+        }
+
+        void Client_OnAuthError(object sender, System.Xml.XmlElement rp)
+        {
+            Console.WriteLine("Error logging into XMPP server: {0}", rp.ToString());
+        }
+
+        void Client_OnAuthenticate(object sender)
+        {
+            ThreadPool.QueueUserWorkItem(sync =>
+                {
+                    Console.WriteLine("XMPP logged in to " + Client.NetworkHost);
+                    foreach (string addr in Conf.Conferences.Values)
+                    {
+                        try
+                        {
+                            Room room = ConfManager.GetRoom(addr + "/" + Conf.User);
+                            room.OnJoin += new RoomEvent(room_OnJoin);
+                            room.OnLeave += new RoomPresenceHandler(room_OnLeave);
+                            room.Join();
+                        }
+                        catch (Exception ex)
+                        {
+                            Console.WriteLine("Error joining conference {0}: {1}", addr, ex.Message);
+                        }
+                    }
+                }
+            );
+        }
+
+        void room_OnLeave(Room room, jabber.protocol.client.Presence pres)
+        {
+            if (Conferences.Contains(room))
+                Conferences.Remove(room);
+        }
+
+        void room_OnJoin(Room room)
+        {
+            Console.WriteLine("Joined group chat " + room.RoomAndNick.ToString());
+            room.OnRoomMessage += new MessageHandler(room_OnRoomMessage);
+
+            Conferences.Add(room);
+        }
+
+        void room_OnRoomMessage(object sender, jabber.protocol.client.Message msg)
+        {
+            System.Console.WriteLine("{0}: {1}", msg.From.Resource, msg.Body);
+        }
+
+        private void Client_OnReadText(object sender, string txt)
+        {
+            if (txt != " ")
+                Console.WriteLine("RECV: " + txt);
+        }
+
+        private void Client_OnWriteText(object sender, string txt)
+        {
+            return;
+            if (txt != " ")
+                Console.WriteLine("SENT: " + txt);
+        }
+
+        private void Client_OnError(object sender, Exception ex)
+        {
+            Console.WriteLine("ERROR: " + ex.ToString());
+        }
+
+
+        private void Client_OnStreamError(object sender, System.Xml.XmlElement rp)
+        {
+            Console.WriteLine("Stream ERROR: " + rp.OuterXml);
         }
 
         void Client_OnConnect(object sender, StanzaStream stream)
         {
-            Stream = stream;
-            Console.WriteLine("XMPP connected to " + Client.Server);
+            Console.WriteLine("XMPP server connected, logging in...");
         }
 
         bool Client_OnInvalidCertificate(object sender, System.Security.Cryptography.X509Certificates.X509Certificate certificate, System.Security.Cryptography.X509Certificates.X509Chain chain, System.Net.Security.SslPolicyErrors sslPolicyErrors)
